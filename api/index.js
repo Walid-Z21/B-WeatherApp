6f94aba6119c86fd48e8782575e4b77f8131f448
GrizzlyBeard <zidouh.walid21@gmail.com>
2026-08-26 21:51:23 +0100

    fixing bugs

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app=express();
 
 app.use(cors({
-    origin: 'https://weather-app-nu-khaki-53.vercel.app'
+    origin: 'https://weather-app-zeta-nine-29.vercel.app/'
 }))
 app.use(express.json())
 
